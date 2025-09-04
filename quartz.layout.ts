69aaf37d9ae0a69d5e232e03271ce262aa4956b1
aria <hello@aria.coffee>
2025-09-04 21:34:20 +1000

    update footer links

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -8,8 +8,8 @@ export const sharedPageComponents: SharedLayout = {
   afterBody: [],
   footer: Component.Footer({
     links: {
-      GitHub: "https://github.com/jackyzha0/quartz",
-      "Discord Community": "https://discord.gg/cRFFHYye7t",
+      GitHub: "https://github.com/BuyMyMojo/trans-info.aria.coffee",
+      "Email me!": "mailto:transinfo@aria.coffee",
     },
   }),
 }
