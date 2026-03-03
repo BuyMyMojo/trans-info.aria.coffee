b01fa52523b3330a728257695c59724fd1d55a26
aria <hello@aria.coffee>
2026-03-03 15:24:13 +1100

    chore: remove unused analytics

--- quartz.config.ts
+++ quartz.config.ts
@@ -12,9 +12,7 @@ const config: QuartzConfig = {
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
-    analytics: {
-      provider: "plausible",
-    },
+    analytics: null,
     locale: "en-US",
     baseUrl: "trans-info.aria.coffee",
     ignorePatterns: ["private", "templates", ".obsidian"],
